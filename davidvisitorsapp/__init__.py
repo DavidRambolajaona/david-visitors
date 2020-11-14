5705ba184efb7f9133cd92554ef70a6316fd3931
DavidRambolajaona <david.rambolajaona@esti.mg>
2020-11-14 06:13:47 +0300

    Base of the app

---
+++ davidvisitorsapp/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+from .home.routes import home_bp
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(home_bp)
+
+    return app
---
+++ run.py
@@ -0,0 +1,5 @@
+from davidvisitorsapp import create_app
+
+if __name__ == "__main__" :
+    app = create_app()
+    app.run(debug=True)
